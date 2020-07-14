a2937d7a3ec78ea6f355c33a8d56eeb68a06cb53
uzudil <hunhm1@gmail.com>
2020-07-13 22:11:22 -0700

    simplified go code

--- svaltfen.go
+++ svaltfen.go
@@ -2,33 +2,18 @@ package main
 
 import (
 	"flag"
-	"os"
 
-	"github.com/uzudil/benji4000/bscript"
-	"github.com/uzudil/benji4000/gfx"
-	"github.com/uzudil/benji4000/sound"
+	"github.com/uzudil/benji4000/core"
 )
 
-func repl(video *gfx.Gfx, sound *sound.Sound) {
-	bscript.Repl(video, sound)
-}
-
 func main() {
-	showAst := flag.Bool("ast", false, "print AST and not execute?")
 	fullscreen := flag.Bool("fullscreen", false, "Run at fullscreen")
+	nosound := flag.Bool("nosound", false, "Run without sound")
 
 	var scale int
 	flag.IntVar(&scale, "scale", 2, "Image scale factor")
-
 	flag.Parse()
 
-	video := gfx.NewGfx(scale, *fullscreen)
-	sound := sound.NewSound()
-
-	go func() {
-		bscript.Run("bscript", showAst, nil, video, sound)
-		os.Exit(0)
-	}()
-
-	video.Render.MainLoop()
+	showAst := false
+	core.Run("bscript", scale, fullscreen, nosound, &showAst)
 }
